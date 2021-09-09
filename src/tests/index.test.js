0f27f8da2586db3ff0220d5ef8fd87489939b0f6
Tom Lamb <tom.lamb@ui.com>
2021-09-09 20:45:45 +0200

    Updates to tests

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -2,13 +2,13 @@ const request = require('supertest')
 const app = require('../../app');
 
 describe('Sanity test', () => {
-  it('should test that 1 === 1', () => {
+  test('1 should equal 1', () => {
     expect(1).toBe(1)
   })
 })
 
 describe('Photos endpoint', () => {
-  it('should return object', async () => {
+  test('should return Hello World', async () => {
     const res = await request(app)
       .get('/api/photos')
     expect(res.statusCode).toEqual(200)
